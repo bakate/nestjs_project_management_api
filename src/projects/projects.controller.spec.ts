47709c156510e7ad9966592c070990b50c1027ab
Bakate <b.ba@startupflow.io>
2024-03-24 23:10:20 +0100

    test(projects-module): add some unit tests to project controller and service

--- src/projects/projects.controller.spec.ts
+++ src/projects/projects.controller.spec.ts
@@ -1,20 +1,110 @@
+import { faker } from '@faker-js/faker';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import mongoose from 'mongoose';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 
 describe('ProjectsController', () => {
   let controller: ProjectsController;
+  let service: ProjectsService;
+
+  const userId = new mongoose.Types.ObjectId().toString();
+  const currentProjectId = new mongoose.Types.ObjectId().toString();
+
+  const mockProject = {
+    id: currentProjectId,
+    name: faker.internet.domainName(),
+    description: faker.lorem.sentence(),
+    status: 'active',
+    userId,
+  };
+  const mockUser = {
+    id: userId,
+    name: faker.internet.displayName(),
+    email: faker.internet.email(),
+  };
+
+  const mockProjectService = {
+    create: jest.fn(),
+    findAll: jest.fn().mockResolvedValueOnce([mockProject]),
+    findOne: jest.fn(),
+    findById: jest.fn().mockResolvedValueOnce(mockProject),
+    updateById: jest.fn(),
+    removeById: jest.fn().mockResolvedValueOnce({ deleted: true }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
       controllers: [ProjectsController],
-      providers: [ProjectsService],
+      providers: [
+        {
+          provide: ProjectsService,
+          useValue: mockProjectService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ProjectsController>(ProjectsController);
+    service = module.get<ProjectsService>(ProjectsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should get all projects', async () => {
+    const result = await controller.findAll();
+
+    expect(result).toEqual([mockProject]);
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should get a project by id', async () => {
+    const result = await controller.findById(mockProject.id);
+
+    expect(result).toEqual(mockProject);
+    expect(service.findById).toHaveBeenCalledWith(mockProject.id);
+  });
+
+  it('should update a project', async () => {
+    const updateProjectDto = {
+      ...mockProject,
+      description: faker.lorem.sentence(),
+    };
+    mockProjectService.updateById = jest
+      .fn()
+      .mockResolvedValueOnce(updateProjectDto);
+
+    const result = await controller.updateById(
+      mockProject.id,
+      updateProjectDto,
+    );
+
+    expect(result).toEqual(updateProjectDto);
+  });
+
+  it('should remove a project', async () => {
+    const result = await controller.removeById(mockProject.id);
+
+    expect(result).toEqual({ deleted: true });
+    expect(service.removeById).toHaveBeenCalledWith(mockProject.id);
+  });
+
+  it('should create a project', async () => {
+    const createProjectDto = {
+      name: mockProject.name,
+      description: mockProject.description,
+      status: mockProject.status,
+    };
+    mockProjectService.create = jest.fn().mockResolvedValueOnce(mockProject);
+
+    const result = await controller.create({
+      ...createProjectDto,
+      userId: mockUser.id,
+    });
+
+    expect(result).toEqual(mockProject);
+    expect(service.create).toHaveBeenCalledWith({
+      ...createProjectDto,
+      userId: mockUser.id,
+    });
   });
 });
--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,18 +1,156 @@
+import { faker } from '@faker-js/faker';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import mongoose, { Model } from 'mongoose';
+import { CreateProjectDto } from './dto/project.dto';
 import { ProjectsService } from './projects.service';
+import { Project } from './schemas/project.schema';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
+  let model: Model<Project>;
+  const userId = new mongoose.Types.ObjectId().toString();
+  const currentProjectId = new mongoose.Types.ObjectId().toString();
+
+  const mockProject = {
+    id: currentProjectId,
+    name: faker.internet.domainName(),
+    description: faker.lorem.sentence(),
+    status: 'active',
+    userId,
+  };
+  const mockUser = {
+    id: userId,
+    name: faker.internet.displayName(),
+    email: faker.internet.email(),
+  };
+
+  const mockProjectService = {
+    create: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findById: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectsService],
+      providers: [
+        ProjectsService,
+        {
+          provide: getModelToken(Project.name),
+          useValue: mockProjectService,
+        },
+      ],
     }).compile();
 
     service = module.get<ProjectsService>(ProjectsService);
+    model = module.get<Model<Project>>(getModelToken(Project.name));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of projects', async () => {
+      const expectedOutput = [mockProject];
+      jest.spyOn(model, 'find').mockImplementation(() => expectedOutput as any);
+      const result = await service.findAll();
+
+      expect(result).toBe(expectedOutput);
+      expect(model.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a project', async () => {
+      const newProject = {
+        name: faker.lorem.words(),
+        description: faker.lorem.sentence(),
+        status: 'active',
+      };
+      const expectedOutput = mockProject;
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.resolve(mockProject as any));
+
+      const result = await service.create({
+        ...newProject,
+        userId: mockUser.id,
+      } as CreateProjectDto);
+      expect(result).toBe(expectedOutput);
+      expect(model.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findById', () => {
+    it('should find and return a project by ID', async () => {
+      jest.spyOn(model, 'findById').mockResolvedValue(mockProject as any);
+
+      const result = await service.findById(mockProject.id);
+
+      expect(result).toEqual(mockProject);
+      expect(model.findById).toHaveBeenCalledWith(mockProject.id);
+    });
+
+    it('should throw NotFoundException if project is not found', async () => {
+      const fakeProjectId = 'invalid-id';
+      jest.spyOn(mongoose, 'isValidObjectId').mockReturnValue(true);
+      jest.spyOn(model, 'findById').mockResolvedValue(null);
+
+      await expect(service.findById(fakeProjectId)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should throw BadRequestException if id is invalid', async () => {
+      const fakeProjectId = 'invalid-id';
+      jest.spyOn(mongoose, 'isValidObjectId').mockReturnValue(false);
+
+      await expect(service.findById(fakeProjectId)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should update a project', async () => {
+      const updatedProject = {
+        name: faker.lorem.words(),
+        description: faker.lorem.sentence(),
+        status: 'active',
+      };
+      jest
+        .spyOn(model, 'findByIdAndUpdate')
+        .mockResolvedValue(mockProject as any);
+
+      const result = await service.updateById(
+        mockProject.id,
+        updatedProject as any,
+      );
+
+      expect(result).toEqual(mockProject);
+      expect(model.findByIdAndUpdate).toHaveBeenCalledWith(
+        mockProject.id,
+        updatedProject,
+        { new: true },
+      );
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a project', async () => {
+      jest
+        .spyOn(model, 'findByIdAndDelete')
+        .mockResolvedValue(mockProject as any);
+
+      const result = await service.removeById(mockProject.id);
+
+      expect(result).toEqual(mockProject);
+      expect(model.findByIdAndDelete).toHaveBeenCalledWith(mockProject.id);
+    });
   });
 });
